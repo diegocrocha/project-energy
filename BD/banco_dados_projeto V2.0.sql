53d18d50a3d113300e1ab0381f976c84521c2dd7
MarkzBandtec <vitor.portela@bandtec.com.br>
2020-03-09 21:54:34 -0300

    Downgrade no banco de dados.

---
+++ BD/banco_dados_projeto V2.0.sql
@@ -0,0 +1,43 @@
+create database 7_energy;
+
+use 7_energy;
+
+create table dado_empresas ( 
+	cnpj char(14) primary key,
+    empresa varchar(40),
+    email varchar(40),
+    senha varchar(16)
+    );
+
+Create table login_secundario (
+	cnpj char(14) primary key,
+    Login varchar(20),
+    Senha varchar(16)
+);
+
+Create table setores (
+	id_setor int primary key,
+    setor varchar(40),
+    id_arduino int,
+    Lumen_padrao int,
+    cnpj int
+);
+
+create table arduinos (
+	id_arduino int primary key,
+    dma char(8),
+    hora char(4),
+    watts int,
+    setor int
+);
+
+create table registro_arduino (
+	id_registro int primary key,
+    dia char(2),
+    mes char(2),
+    ano char(4),
+    Hora char(4),
+    watts int,
+    id_arduino int
+);
+
